464e7846d23bc6f099e8df0cd57081bd966730a4
NaKa2355 <24926264+NaKa2355@users.noreply.github.com>
2023-03-24 05:14:29 +0900

    add oneof to irdata

--- gen/go/common/irdata/v1/raw_irdata.pb.go
+++ gen/go/common/irdata/v1/raw_irdata.pb.go
@@ -75,6 +75,73 @@ func (x *RawIrData) GetOnOffPluseNs() []uint32 {
 	return nil
 }
 
+type IRData struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Types that are assignable to Data:
+	//
+	//	*IRData_Raw
+	Data isIRData_Data `protobuf_oneof:"data"`
+}
+
+func (x *IRData) Reset() {
+	*x = IRData{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_common_irdata_v1_raw_irdata_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *IRData) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*IRData) ProtoMessage() {}
+
+func (x *IRData) ProtoReflect() protoreflect.Message {
+	mi := &file_common_irdata_v1_raw_irdata_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use IRData.ProtoReflect.Descriptor instead.
+func (*IRData) Descriptor() ([]byte, []int) {
+	return file_common_irdata_v1_raw_irdata_proto_rawDescGZIP(), []int{1}
+}
+
+func (m *IRData) GetData() isIRData_Data {
+	if m != nil {
+		return m.Data
+	}
+	return nil
+}
+
+func (x *IRData) GetRaw() *RawIrData {
+	if x, ok := x.GetData().(*IRData_Raw); ok {
+		return x.Raw
+	}
+	return nil
+}
+
+type isIRData_Data interface {
+	isIRData_Data()
+}
+
+type IRData_Raw struct {
+	Raw *RawIrData `protobuf:"bytes,1,opt,name=raw,proto3,oneof"`
+}
+
+func (*IRData_Raw) isIRData_Data() {}
+
 var File_common_irdata_v1_raw_irdata_proto protoreflect.FileDescriptor
 
 var file_common_irdata_v1_raw_irdata_proto_rawDesc = []byte{
@@ -86,11 +153,15 @@ var file_common_irdata_v1_raw_irdata_proto_rawDesc = []byte{
 	0x28, 0x0d, 0x52, 0x0e, 0x63, 0x61, 0x72, 0x72, 0x69, 0x65, 0x72, 0x46, 0x72, 0x65, 0x71, 0x4b,
 	0x68, 0x7a, 0x12, 0x25, 0x0a, 0x0f, 0x6f, 0x6e, 0x5f, 0x6f, 0x66, 0x66, 0x5f, 0x70, 0x6c, 0x75,
 	0x73, 0x65, 0x5f, 0x6e, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0d, 0x52, 0x0c, 0x6f, 0x6e, 0x4f,
-	0x66, 0x66, 0x50, 0x6c, 0x75, 0x73, 0x65, 0x4e, 0x73, 0x42, 0x3a, 0x5a, 0x38, 0x67, 0x69, 0x74,
-	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x4e, 0x61, 0x4b, 0x61, 0x32, 0x33, 0x35, 0x35,
-	0x2f, 0x69, 0x72, 0x64, 0x65, 0x63, 0x6b, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x65,
-	0x6e, 0x2f, 0x67, 0x6f, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2f, 0x69, 0x72, 0x64, 0x61,
-	0x74, 0x61, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x66, 0x66, 0x50, 0x6c, 0x75, 0x73, 0x65, 0x4e, 0x73, 0x22, 0x37, 0x0a, 0x06, 0x49, 0x52, 0x44,
+	0x61, 0x74, 0x61, 0x12, 0x25, 0x0a, 0x03, 0x72, 0x61, 0x77, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b,
+	0x32, 0x11, 0x2e, 0x69, 0x72, 0x64, 0x61, 0x74, 0x61, 0x2e, 0x52, 0x61, 0x77, 0x49, 0x72, 0x44,
+	0x61, 0x74, 0x61, 0x48, 0x00, 0x52, 0x03, 0x72, 0x61, 0x77, 0x42, 0x06, 0x0a, 0x04, 0x64, 0x61,
+	0x74, 0x61, 0x42, 0x3a, 0x5a, 0x38, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
+	0x2f, 0x4e, 0x61, 0x4b, 0x61, 0x32, 0x33, 0x35, 0x35, 0x2f, 0x69, 0x72, 0x64, 0x65, 0x63, 0x6b,
+	0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x67, 0x6f, 0x2f, 0x63, 0x6f,
+	0x6d, 0x6d, 0x6f, 0x6e, 0x2f, 0x69, 0x72, 0x64, 0x61, 0x74, 0x61, 0x2f, 0x76, 0x31, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -105,16 +176,18 @@ func file_common_irdata_v1_raw_irdata_proto_rawDescGZIP() []byte {
 	return file_common_irdata_v1_raw_irdata_proto_rawDescData
 }
 
-var file_common_irdata_v1_raw_irdata_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_common_irdata_v1_raw_irdata_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_common_irdata_v1_raw_irdata_proto_goTypes = []interface{}{
 	(*RawIrData)(nil), // 0: irdata.RawIrData
+	(*IRData)(nil),    // 1: irdata.IRData
 }
 var file_common_irdata_v1_raw_irdata_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: irdata.IRData.raw:type_name -> irdata.RawIrData
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_common_irdata_v1_raw_irdata_proto_init() }
@@ -135,6 +208,21 @@ func file_common_irdata_v1_raw_irdata_proto_init() {
 				return nil
 			}
 		}
+		file_common_irdata_v1_raw_irdata_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*IRData); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
+	file_common_irdata_v1_raw_irdata_proto_msgTypes[1].OneofWrappers = []interface{}{
+		(*IRData_Raw)(nil),
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -142,7 +230,7 @@ func file_common_irdata_v1_raw_irdata_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_common_irdata_v1_raw_irdata_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
